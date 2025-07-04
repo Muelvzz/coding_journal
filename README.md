9ae3588f58788922ae74fa49b4512638eb66b9b7
Muelvin Lopez <muelvinlopez25@gmail.com>
2025-07-04 09:48:10 +0800

    seperated backedn and frontend functions

--- README.md
+++ README.md
@@ -3,14 +3,15 @@
 This program is heavily inspired by mubeen161 Thought Journal (https://github.com/mubeen161/thought_journal)
 
 ## Library requirements
-Please download these following libraries for the program to work
-* streamlit
-* datetime
-* sqlite3
+Please download these following libraries for the program to work:
+    import streamlit as st
+    from datetime import datetime
+    from nltk.sentiment import SentimentIntensityAnalyzer
+    import plotly.express as px
 
 ## Important Notice
-Please remember, the program is still small and I know that I haven't done something compelling with this program, but I hope that by doing this. I get to improve my craft of coding as I create more and more projects. There is always a room for improvement and this is certainly one of them - in fact, I am adding this section right now to tell you on some features that I want to add on to this besides that missing "edit" feature
+Please remember, the program is still small and I know that I haven't done something compelling with this program, but I hope that by doing this. I get to improve my craft of coding as I create more and more projects. There is always a room for improvement and this is certainly one of them - in fact, I am adding this section right now to tell you on some features that I want to add on to this. Now, as of writing this on friday at the fourth of July. I think I can finally say that I have done my best for my very first project. 
 
-If you have any comments and suggestions. Please let me know, at my TikTok, Linkedin, Facebook or Instagram:
+If you have any comments and suggestions. Please let me know, at my TikTok, Linkedin:
 Tiktok: https://www.tiktok.com/@muelvz?_t=ZS-8xgT713yjSR&_r=1
 Linkedin: https://www.linkedin.com/in/psalm-muelvin-lopez-003a78227?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app
--- main.py
+++ main.py
@@ -1,71 +1,6 @@
 import streamlit as st
-import sqlite3
 from datetime import datetime
-from nltk.sentiment import SentimentIntensityAnalyzer
-import plotly.express as px
-
-class Journal():
-
-    # To be honest, this feels wrong to have this code pass
-    def __init__(self):
-        pass
-
-    """ Since this program uses sqlite as a database. You have to create your own sqlite database. make sure that your sqlite table contains this following columns:
-            id - integer, auto-increment, primary key
-            date - text
-            journal - text"""
-    def add_journal(self, get_timestamp, get_diary):
-        with sqlite3.connect("database.db") as conn:
-            cursor = conn.cursor()
-
-            #Always specify the column names in your INSERT statement when not providing all columns, especially when using auto-increment primary keys.
-
-            cursor.execute("INSERT INTO diaries (date, journal) VALUES (?, ?)", (get_timestamp, get_diary))
-            conn.commit()
-
-    def view_journal(self):
-        st.markdown(
-        "<div style='text-align: center;'>"
-        "<h3>Journal Entry</h3>"
-        "</div>",
-        unsafe_allow_html=True)
-
-        with sqlite3.connect("database.db") as conn:
-            cursor = conn.cursor()
-            cursor.execute("SELECT * FROM diaries")
-            result = cursor.fetchall()
-            return result
-
-    """ There is some errors into this, a small minor issue and that is if you are going to delete an entry. You have to press it twice so that it will change. I've tried adding experimental rerun() into this to fix it. But streamlit produces an error saying that there no such thing as that function (my streamlit is up-to-date btw.)"""
-    def delete_journal(self, get_id):
-        with sqlite3.connect("database.db") as conn:
-            cursor = conn.cursor()
-            cursor.execute("DELETE FROM diaries WHERE id = ?", (get_id,))
-            conn.commit()
-
-    def graphing(self):
-
-        analyzer = SentimentIntensityAnalyzer()
-        date_list = []
-        diary_list = []
-
-        with sqlite3.connect("database.db") as conn:
-            cursor = conn.cursor()
-            cursor.execute("SELECT date, journal FROM diaries")
-            result = cursor.fetchall()
-
-            for data in result:
-                scores = analyzer.polarity_scores(data[1])
-                overall_score = scores["compound"]
-
-                date_list.append(data[0])
-                diary_list.append(overall_score)
-
-            figure = px.line(x=date_list, y=diary_list, labels={"x" : "Dates", "y" : "Scores"})
-            st.plotly_chart(figure)
-
-            return figure
-    
+from functions import Journal
 
 def Frontend():
 
@@ -100,14 +35,15 @@ def Frontend():
     if st.session_state.view_mode:
         journal_entries = st.session_state.journal.view_journal()
         if journal_entries:
-            for index, journal_entry in enumerate(journal_entries, start=1):
-                id = journal_entry[0]
 
+            for index, journal_entry in enumerate(journal_entries, start=1):
+
+                id = journal_entry[0]
                 st.markdown(f"<h3>Day {index}:</h3>", unsafe_allow_html=True)
                 st.markdown(f"<b>{journal_entry[1]}:</b>", unsafe_allow_html=True)
                 st.write(f"{journal_entry[2]}")
-                
-                delete_clicked = st.button("Delete", key=f"delete_{id}")
+                    
+                delete_clicked = st.button("Delete", key=f"delete_{index}")
 
                 if delete_clicked:
                     st.session_state.journal.delete_journal(id)
