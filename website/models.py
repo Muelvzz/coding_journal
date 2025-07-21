07852c0f089b8feffafc39afe4e78ad3f0470a82
Muelvin Lopez <muelvinlopez25@gmail.com>
2025-07-21 20:27:42 +0800

    showed journal entries on home

--- website/models.py
+++ website/models.py
@@ -8,3 +8,12 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+
+    posts = db.relationship("Journal", backref="user", passive_deletes=True)
+
+class Journal(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    author = db.Column(db.Integer, db.ForeignKey("user.id", ondelete="CASCADE"), nullable=False)
--- website/views.py
+++ website/views.py
@@ -1,17 +1,40 @@
-from flask import Flask, render_template, Blueprint
-from flask_login import login_required
+from flask import Flask, render_template, Blueprint, request, flash, redirect, url_for
+from flask_login import login_required, current_user
+from .models import User, Journal
+from . import db
 
 views = Blueprint("views", __name__)
 
 @views.route("/")
 @login_required
 def home():
-    return render_template("home.html")
 
-@views.route("/add")
+    journals = Journal.query.all()
+
+    return render_template("home.html", user=current_user, journals=journals)
+
+@views.route("/add", methods=["POST", "GET"])
 @login_required
 def add_journal():
-    return render_template("add_journal.html")
+
+    if request.method == "POST":
+        title = request.form.get("text_title")
+        content = request.form.get("text")
+
+        if not title:
+            flash("Your post lacks a title", category="error")
+
+        elif not content:
+            flash("Your post lacks a content", category="error")
+
+        else:
+            journal = Journal(content=content, title=title, author=current_user.id)
+            db.session.add(journal)
+            db.session.commit()
+
+            return redirect(url_for("views.home"))
+
+    return render_template("add_journal.html", user=current_user)
 
 @views.route("/about")
 def about():
