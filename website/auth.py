2c845c0044a0b63caf76b272d367beeadba770ff
Muelvin Lopez <muelvinlopez25@gmail.com>
2025-07-29 19:19:57 +0800

    add verification code

--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, Blueprint, request, flash, redirect, url_for
+from flask import Flask, render_template, Blueprint, request, flash, redirect, url_for, session
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from flask_login import login_user, current_user, login_required, logout_user
+import smtplib
+import random
 
 auth = Blueprint("auth", __name__)
 
@@ -52,18 +54,63 @@ def sign_up():
         elif len(password1) < 8:
             flash("Password must be at least eight characters long", category="error")
         else:
-            new_user = User(email=email,
-                            username=username,
-                            password=generate_password_hash(password2, method="pbkdf2:sha256"))
-            db.session.add(new_user)
-            db.session.commit()
+            session["email"] = email
+            session["username"] = username
+            session["password"] = password2
 
-            login_user(new_user, remember=True)
-            flash("User is created", category="success")
+            random_code = random.randint(100000, 999999)
+            sender_email = "muelvinlopez25@gmail.com"
 
-            return redirect(url_for("views.home"))
+            subject = "Code Verification for CodingJourn"
+            message = f"Thanks for logging in on my website. Your verification code is {random_code}"
+
+            text = f"Subject: {subject}\n\n{message}"
+
+            server = smtplib.SMTP("smtp.gmail.com", 587)
+            server.starttls()
+
+            server.login(sender_email, "bphe trqd tprv jvbm")
+            server.sendmail(sender_email, email, text)
+
+            session["code"] = random_code
+
+            print(f"The first print {session['code']}")
+
+            return redirect(url_for("auth.user_verify"))
 
     return render_template("sign-up.html", user=current_user)
+
+@auth.route("/verify", methods=["GET", "POST"])
+def user_verify():
+    receiver_email = session["email"]
+
+    if "email" in session:
+        code = session["code"]
+
+        if request.method == "POST":
+            get_user_code = request.form.get("verification")
+
+            print(f"The second print {code}")
+
+            if get_user_code == code:
+                receiver_email = session["email"]
+                receiver_password = session["password"]
+                receiver_username = session["username"]
+
+                new_user = User(email=receiver_email,
+                                username=receiver_username,
+                                password=generate_password_hash(receiver_password, method="pbkdf2:sha256"))
+                db.session.add(new_user)
+                db.session.commit()
+
+                login_user(new_user, remember=True)
+                flash("User is created", category="success")
+                return redirect(url_for("views.home"))
+
+            else:
+                flash("Incorrect code input", category="error")
+
+    return render_template("verify.html", email=receiver_email)
 
 @auth.route("/logout")
 @login_required
